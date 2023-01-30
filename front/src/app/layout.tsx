ecb82f9a7ba230af73ff8a6ed01a10d4e19a1d2e
mrsn7 <sena-mori@juaarchi.co.jp>
2023-01-30 15:47:26 +0900

    テーマ変更

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,20 +2,18 @@
 import './globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import theme from '../theme/theme';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="ja">
       <head />
       <body>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           {children}
         </ChakraProvider>
       </body>
