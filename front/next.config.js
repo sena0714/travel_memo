ba54c540b378885de21a41ab7b6c275f725e2ee4
mrsn7 <sena-mori@juaarchi.co.jp>
2023-02-07 09:42:50 +0900

    動作速度改善の為、ポーリング間隔を長くした

--- front/next.config.js
+++ front/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 5000,
+      aggregateTimeout: 300,
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
