bd8d1b982402b331c58d9a1d7bdac302ce4c2f44
sena0714 <m.s.5241@ezweb.ne.jp>
2023-02-08 23:21:05 +0900

    修正不可なエラーに遭遇したのでaxiosに戻す

--- front/src/app/users/page.tsx
+++ front/src/app/users/page.tsx
@@ -1,4 +1,6 @@
 import React, { use } from "react";
+import { AxiosResponse } from 'axios';
+import { axiosApiFromServerSide } from '@/lib/axios';
 import { Text } from "@/components/ChakraComponents";
 
 import UserDetail from '@/components/UserDetail';
@@ -10,25 +12,22 @@ type User = {
 };
 
 async function fetchUsers(): Promise<User[]> {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL_FROM_SERVER_SIDE+'/api/users');
-    
-    if (!res.ok) {
-            throw new Error("Failed to fetch users");
-    }
-
-    const data = await res.json();
-    return data.data as User[];
+    const response: AxiosResponse = await axiosApiFromServerSide.get('/api/users');
+    const users: User[] = response.data.data;
+    return users;
 }
 
-export default async function Users() {
-    const users = await fetchUsers();
+export default function Users() {
+    const users = use(fetchUsers());
 
     return (
         <main>
             <Text fontSize='2xl' fontWeight='bold'>ユーザー一覧</Text>
-            {users && users.map((user: User) => (
-                <UserDetail key={user.id} user={user} />
-            ))}
+            {users && users.map((user: User) => {
+                return (
+                    <UserDetail key={user.id} user={user} />
+                );
+            })}
         </main>
     );
 }
